592f60eda3fd6b902b9eeff84040e59dc9db9dd0
Arash Khajavi <arash.khajavi.ghk123@gmail.com>
2025-06-15 17:03:20 +0300

    Update a.py

--- a.py
+++ a.py
@@ -82,4 +82,5 @@ def IsraelGPT():
         return "Error parsing response", e, "Raw Response - ", raw_response
  return render_template("fl.html")
 if __name__ == "__main__":
-  app.run()
+   port = int(os.environ.get("PORT", 5000))
+   app.run(host="0.0.0.0", port=port)
