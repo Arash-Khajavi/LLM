0baac88c3ac442d0b2de1cfa872fe4443e8f65cd
Arash Khajavi <arash.khajavi.ghk123@gmail.com>
2025-10-08 10:22:41 +0330

    Update tools.py

--- tools.py
+++ tools.py
@@ -11,7 +11,7 @@ from pydantic import BaseModel
 import requests
 from bs4 import BeautifulSoup  # ✅ Correct
 # Define the argument schema for the tool
-from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
+from langchain_community.tools import WikipediaQueryRun
 from langchain_community.utilities import WikipediaAPIWrapper
 from langchain.tools import Tool
 from datetime import datetime
@@ -33,12 +33,7 @@ save_tool = Tool(
     description="Saves structured research data to a text file.",
 )
 
-search = DuckDuckGoSearchRun()
-search_tool = Tool(
-    name="search",
-    func=search.run,
-    description="Search the web for information",
-)
+
 
 api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=100)
 wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
