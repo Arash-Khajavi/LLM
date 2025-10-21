677e7e5ca60cf45473be3288ea0d6bc9d423aee7
Arash Khajavi <arash.khajavi.ghk123@gmail.com>
2025-10-22 00:13:16 +0330

    Update a.py

--- a.py
+++ a.py
@@ -6,7 +6,7 @@ from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import PydanticOutputParser
-from langchain.agents import create_openai_functions_agent, AgentExecutor
+from langchain.agents import create_tool_functions_agent, AgentExecutor
 from tools import save_tool, AquilaTool
 from flask import Flask,render_template,redirect,url_for,request,jsonify
 import random
